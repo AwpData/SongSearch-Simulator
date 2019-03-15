bd7c96c13c8058e46a6189aa663ab9024842b0cb
AwpData <awpdata@gmail.com>
2019-03-14 18:46:47 -0700

    Update Range.java

--- Range.java
+++ Range.java
@@ -1,4 +1,29 @@
-
 public class Range {
+	private int min;
+	private int max;
 
+	public static Range parse(String s) {
+		Range range = new Range();
+		if (!s.contains("-")) {
+			range.min= Integer.valueOf(s);
+			range.max = range.min;
+		} else {
+			range.min = Integer.valueOf(s.substring(0, s.indexOf("-")));
+			range.max = Integer.valueOf(s.substring(s.indexOf("-") + 1));
+		}
+		if (range.min > range.max) {
+			int temp = range.max;
+			range.max = range.min;
+			range.min = temp;
+		}
+		return range;
+	}
+
+	public int getMin() {
+		return min;
+	}
+
+	public int getMax() {
+		return max;
+	}
 }
