eac5a101bfecb04aea28c153fb4a8ea5e2679a0e
AwpData <awpdata@gmail.com>
2019-03-18 11:07:07 -0700

    Update Song.java

--- Song.java
+++ Song.java
@@ -7,7 +7,7 @@ public class Song {
 	private String songname;
 	public static ArrayList<Song> songs = new ArrayList<>();
 
-	public static Song parse(String s) { // "constructor"
+	public static Song parse(String s) { // Parse a string of the form “Year\tRank\tArtist\tTitle” and create a Song object with the given values.
 		Scanner parse = new Scanner(s).useDelimiter("\t");
 		Song song = new Song();
 		song.year = parse.nextInt();
@@ -22,23 +22,23 @@ public class Song {
 		new SongCollection(songs);
 	}
 
-	public int getYear() {
+	public int getYear() { // Return the year of this song.
 		return year;
 	}
 
-	public int getRank() {
+	public int getRank() { // Return the rank of this song.
 		return rank;
 	}
 
-	public String getArtist() {
+	public String getArtist() { // Return the artist of this song.
 		return artist;
 	}
 
-	public String getTitle() {
+	public String getTitle() { // Return the title of this song.
 		return songname;
 	}
 
-	public String toString() {
-		return (getYear() + "\t" + getRank() + "\t" + getArtist() + "\t\t" + getTitle());
+	public String toString() { // Return a string representation of this song.
+		return (getYear() + "\t" + getRank() + "\t" + getArtist() + "\t\t\t\t\t\t" + getTitle());
 	}
 }
