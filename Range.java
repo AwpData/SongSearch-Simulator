da65c7c7be05513585c32e259916744dd5f08d36
AwpData <awpdata@gmail.com>
2019-03-18 11:07:56 -0700

    Update Range.java

--- Range.java
+++ Range.java
@@ -2,7 +2,7 @@ public class Range {
 	private int min;
 	private int max;
 
-	public static Range parse(String s) {
+	public static Range parse(String s) { // Parse a string of the form "<min>/<max>" or "<value>" and create a Range representing those values
 		Range range = new Range();
 		if (!s.contains("/")) {
 			range.min = Integer.valueOf(s);
@@ -19,11 +19,31 @@ public class Range {
 		return range;
 	}
 
-	public int getMin() {
+	public boolean contains(int n) { // Returns true if the given value is in this range, and false otherwise.
+		if (n >= this.getMin() && n <= this.getMax()) {
+			if (n == this.getMin()) {
+				System.out.println(n + " is equal to " + this.getMin());
+				return true;
+			}
+			if (n == this.getMax()) {
+				System.out.println(n + " is equal to " + this.getMax());
+				return true;
+			} else {
+				System.out.println(n + " is between " + this.getMin() + " and " + this.getMax());
+				return true;
+			}
+		} else {
+			System.out.println(n + " is not between " + this.getMin() + " and " + this.getMax());
+			return false;
+		}
+	}
+
+	public int getMin() { // Return the minimum value in this range.
 		return min;
 	}
 
-	public int getMax() {
+	public int getMax() { // Return the maximum value in this range.
 		return max;
 	}
+
 }
