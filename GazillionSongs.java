fcc07ec6f5f52627dd678ac41d52dccf92fb0ae1
AwpData <awpdata@gmail.com>
2019-03-17 17:53:45 -0700

    Update GazillionSongs.java

--- GazillionSongs.java
+++ GazillionSongs.java
@@ -1,20 +1,22 @@
 import java.util.*;
 import java.io.*;
 
-// Can parse each song
-// Can sort
-// Can get range of years (needs ranks)
+//Needs to print error input
+//Enter INPUT file as MySongs or you will never exit the loop! (For Testing)
+
 public class GazillionSongs {
 	public static void main(String[] args) throws FileNotFoundException {
 		ArrayList<Song> songs = new ArrayList<>();
-		// File testifexists = new File("src/GazillionSongs.txt");
 		Scanner input = new Scanner(System.in);
 		String choice = "";
 		Scanner file = new Scanner(new File("src/GazillionSongs.txt"));
 		System.out.println("Welcome to the program!\nEnter the name of your sick playlist:");
 		String filename = input.nextLine(); // Placeholder that hides the actual name
-
-		System.out.println("Loading " + filename + ".txt...\n");
+		while (!filename.equals("MySongs")) { // TEST LOOP REMOVE LATER
+			System.out.println("File does not exist! Try again!");
+			filename = input.nextLine();
+		}
+		System.out.println("File found! Loading " + filename + ".txt...\n");
 		while (file.hasNextLine()) { // Adds songs to temp array
 			String s = file.nextLine();
 			songs.add(Song.parse(s));
@@ -23,7 +25,7 @@ public class GazillionSongs {
 		System.out.println(filename + ".txt loaded!"); // Psst! Non-existent file
 		System.out.println(songs.size() + " songs loaded!");
 		System.out.println();
-		System.out.println("Enter the name of the file you will output to");
+		System.out.println("Creating File...Enter the name of the file you will output to:");
 		String outputfilename = input.nextLine();
 		System.out.println(outputfilename + ".txt created!");
 
@@ -35,8 +37,8 @@ public class GazillionSongs {
 			System.out.println();
 			System.out.println("What would you like to do with your playlist?");
 			System.out.println("-print (Prints the playlist to console)");
-			System.out.println("-filteryear [YEAR1-YEAR2] or [YEAR] (Filters songs between one or more years)");
-			System.out.println("-filterrank [RANK#-RANK#] or [RANK#] (Filters songs with this rank(s))");
+			System.out.println("-filteryear [YEAR1/YEAR2] or [YEAR] (Filters songs between one or more years)");
+			System.out.println("-filterrank [RANK#/RANK#] or [RANK#] (Filters songs with this rank(s))");
 			System.out.println("-filterartist [ARTIST NAME] (Filters songs by this artist)");
 			System.out.println("-filtertitle [SONG NAME] (Filters songs that have this keyword in it)");
 			System.out.println("-sortByA [year] or [rank] or [artist] or [title] (sorts by \"year\", \"rank\", \"artist\", or \"song\" in acsending order)");
@@ -44,28 +46,40 @@ public class GazillionSongs {
 			System.out.println("-quit (Quits the program)");
 			System.out.println();
 			choice = input.nextLine();
-			if (choice.equals("-print")) {
-				SongCollection.printSongCollection();
-			} else if (choice.contains("-filteryear")) {
-				Range range = Range.parse(choice.substring(12));
-				SongCollection.filterYear(range);
-			} else if (choice.contains("-filterrank")) {
-				Range range = Range.parse(choice.substring(12));
-				SongCollection.filterRank(range);
-			} else if (choice.contains("-filterartist")) {
-				SongCollection.filterArtist(choice.substring(14));
-			} else if (choice.contains("-filtertitle")) {
-				SongCollection.filterTitle(choice.substring(13));
-			} else if (choice.contains("-sortByA year")) {
-				SongCollection.sortYear();
-			} else if (choice.contains("-sortByA rank")) {
-				SongCollection.sortRank();
-			} else if (choice.contains("-sortByA artist")) {
-				SongCollection.sortArtist();
-			} else if (choice.contains("-sortByA title")) {
-				SongCollection.sortTitle();
-			} else if (choice.contains("-restart")) {
-				SongCollection.restart(songs);
+			Scanner split = new Scanner(choice).useDelimiter("\\s-");
+			boolean invalid = false;
+			while (split.hasNext() && !invalid) {
+				String word = split.next();
+				if (word.contains("print")) {
+					System.out.println();
+					SongCollection.printSongCollection();
+				} else if (word.contains("filteryear")) {
+					Range range = Range.parse(word.substring((word.indexOf("filteryear") + 11), word.length()));
+					SongCollection.filterYear(range);
+				} else if (word.contains("filterrank")) {
+					Range range = Range.parse(word.substring((word.indexOf("filterrank") + 11), word.length()));
+					SongCollection.filterRank(range);
+				} else if (word.contains("filterartist")) {
+					SongCollection.filterArtist(word.substring((word.indexOf("filterartist") + 13), word.length()));
+				} else if (word.contains("filtertitle")) {
+					SongCollection.filterTitle(word.substring((word.indexOf("filtertitle") + 12), word.length()));
+				} else if (word.contains("sortByA year")) {
+					SongCollection.sortYear();
+				} else if (word.contains("sortByA rank")) {
+					SongCollection.sortRank();
+				} else if (word.contains("sortByA artist")) {
+					SongCollection.sortArtist();
+				} else if (word.contains("sortByA title")) {
+					SongCollection.sortTitle();
+				} else if (word.contains("restart")) {
+					SongCollection.restart(songs);
+					System.out.println("Restored playlist!");
+				} else if (word.contains("quit")) {
+					break;
+				} else {
+					invalid = true;
+					System.out.println("Invalid Input! Try again");
+				}
 			}
 		}
 		System.out.println("Jam ON!");
